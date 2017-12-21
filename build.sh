c146fd063f1716983e3895b556db1758451509ce
Auston Stewart <auston@shedbuilt.net>
2017-12-21 13:15:25 -1000

    Making symlinks to /run and /run/lock relative to prevent issues when installing to non-root destinations

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ mkdir -v  ${SHED_FAKEROOT}/usr/{,local/}share/{misc,terminfo,zoneinfo}
 mkdir -v  ${SHED_FAKEROOT}/usr/libexec
 mkdir -pv ${SHED_FAKEROOT}/usr/{,local/}share/man/man{1..8}
 mkdir -v ${SHED_FAKEROOT}/var/{log,mail,spool}
-ln -sv /run ${SHED_FAKEROOT}/var/run
-ln -sv /run/lock ${SHED_FAKEROOT}/var/lock
+ln -sv ../run ${SHED_FAKEROOT}/var/run
+ln -sv ../run/lock ${SHED_FAKEROOT}/var/lock
 mkdir -pv ${SHED_FAKEROOT}/var/{opt,cache,lib/{color,misc,locate},local}
 ln -sv /proc/self/mounts ${SHED_FAKEROOT}/etc/mtab
 install -v -m644 ${SHED_CONTRIBDIR}/passwd ${SHED_FAKEROOT}/etc/passwd
