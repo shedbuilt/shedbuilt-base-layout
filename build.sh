e7d42b803a649d380d2bbe65963fd533dc329d6d
Auston Stewart <auston@shedbuilt.net>
2018-01-23 10:28:48 -1000

    Updating for 64-bit builsd

--- build.sh
+++ build.sh
@@ -5,28 +5,36 @@ if [ "$SHED_BUILDMODE" == 'toolchain' ]; then
 fi
 
 # Create essential directories, files and symlinks (from LFS 8.1)
-mkdir -pv ${SHED_FAKEROOT}/{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
-mkdir -pv ${SHED_FAKEROOT}/{media/{floppy,cdrom},sbin,srv,var}
-install -dv -m 0750 ${SHED_FAKEROOT}/root
-install -dv -m 1777 ${SHED_FAKEROOT}/tmp ${SHED_FAKEROOT}/var/tmp
-mkdir -pv ${SHED_FAKEROOT}/usr/{,local/}{bin,include,lib,sbin,src}
-mkdir -pv ${SHED_FAKEROOT}/usr/{,local/}share/{color,dict,doc,info,locale,man}
-mkdir -v  ${SHED_FAKEROOT}/usr/{,local/}share/{misc,terminfo,zoneinfo}
-mkdir -v  ${SHED_FAKEROOT}/usr/libexec
-mkdir -pv ${SHED_FAKEROOT}/usr/{,local/}share/man/man{1..8}
-mkdir -v ${SHED_FAKEROOT}/var/{log,mail,spool}
-ln -sv ../run ${SHED_FAKEROOT}/var/run
-ln -sv ../run/lock ${SHED_FAKEROOT}/var/lock
-mkdir -pv ${SHED_FAKEROOT}/var/{opt,cache,lib/{color,misc,locate},local}
-ln -sv /proc/self/mounts ${SHED_FAKEROOT}/etc/mtab
-install -v -m644 ${SHED_CONTRIBDIR}/passwd ${SHED_FAKEROOT}/etc/passwd
-install -v -m644 ${SHED_CONTRIBDIR}/group ${SHED_FAKEROOT}/etc/group
-touch ${SHED_FAKEROOT}/var/log/{btmp,lastlog,faillog,wtmp}
-chmod -v 664  ${SHED_FAKEROOT}/var/log/lastlog
-chmod -v 600  ${SHED_FAKEROOT}/var/log/btmp
+mkdir -pv "${SHED_FAKEROOT}"/{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
+# Create placeholder 64-bit library folder
+if [[ $SHED_NATIVE_TARGET =~ ^aarch64-.* ]]; then
+    mkdir -v "${SHED_FAKEROOT}/lib64"
+fi
+mkdir -pv "${SHED_FAKEROOT}"/{media/{floppy,cdrom},sbin,srv,var}
+install -dv -m 0750 "${SHED_FAKEROOT}/root"
+install -dv -m 1777 "${SHED_FAKEROOT}/tmp" "${SHED_FAKEROOT}/var/tmp"
+mkdir -pv "${SHED_FAKEROOT}"/usr/{,local/}{bin,include,lib,sbin,src}
+mkdir -pv "${SHED_FAKEROOT}"/usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -v  "${SHED_FAKEROOT}"/usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -v  "${SHED_FAKEROOT}"/usr/libexec
+mkdir -pv "${SHED_FAKEROOT}"/usr/{,local/}share/man/man{1..8}
+mkdir -v  "${SHED_FAKEROOT}"/var/{log,mail,spool}
+mkdir -pv "${SHED_FAKEROOT}"/var/{opt,cache,lib/{color,misc,locate},local}
+touch "${SHED_FAKEROOT}"/var/log/{btmp,lastlog,faillog,wtmp}
+chmod -v 664 "${SHED_FAKEROOT}/var/log/lastlog"
+chmod -v 600 "${SHED_FAKEROOT}/var/log/btmp"
+ln -sv ../run "${SHED_FAKEROOT}/var/run"
+ln -sv ../run/lock "${SHED_FAKEROOT}/var/lock"
+ln -sv /proc/self/mounts "${SHED_FAKEROOT}/etc/mtab"
+install -v -m644 "${SHED_CONTRIBDIR}/passwd" "${SHED_FAKEROOT}/etc/passwd.default"
+install -v -m644 "${SHED_CONTRIBDIR}/group" "${SHED_FAKEROOT}/etc/group.default"
 
 case "$SHED_BUILDMODE" in
     bootstrap)
+        # Install default users and groups
+        install -v -m644 "${SHED_CONTRIBDIR}/passwd" "${SHED_FAKEROOT}/etc/passwd"
+        install -v -m644 "${SHED_CONTRIBDIR}/group" "${SHED_FAKEROOT}/etc/group"
+
         # Create temporary symlinks for bootstrap
         ln -sv /tools/bin/{bash,cat,dd,echo,ln,pwd,rm,stty} "${SHED_FAKEROOT}/bin"
         ln -sv /tools/bin/{env,install,perl} "${SHED_FAKEROOT}/usr/bin"
@@ -59,3 +67,4 @@ case "$SHED_BUILDMODE" in
         chgrp -v utmp "${SHED_FAKEROOT}/var/log/lastlog"
         ;;
 esac
+
--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# HACK: Clean up duplicate symlink that results from installing over an existing system
+if [ -L /run/run ]; then
+    rm /run/run
+fi
+# Install default passwd and group files, if necessary
+if [ ! -e /etc/passwd ]; then
+    install -v -m644 /etc/passwd.default /etc/passwd
+fi
+if [ ! -e /etc/group ]; then
+    install -v -m644 /etc/group.default /etc/group
+fi
 if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
     chgrp -v utmp /var/log/lastlog
 fi
+
