92e8833190e7eeb5264ce7df462476fecb38bc72
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:28:09 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
-if [ "$SHED_BUILDMODE" == 'toolchain' ]; then
+if [ "$SHED_BUILD_MODE" == 'toolchain' ]; then
     echo "The base layout package should not be built in toolchain mode."
     return 1
 fi
 
 # Create essential directories, files and symlinks (from LFS 8.1)
-mkdir -pv "${SHED_FAKEROOT}"/{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
+mkdir -pv "${SHED_FAKE_ROOT}"/{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
 # Create placeholder 64-bit library folder
 if [[ $SHED_NATIVE_TARGET =~ ^aarch64-.* ]]; then
-    mkdir -v "${SHED_FAKEROOT}/lib64"
+    mkdir -v "${SHED_FAKE_ROOT}/lib64"
 fi
-mkdir -pv "${SHED_FAKEROOT}"/{media/{floppy,cdrom},sbin,srv,var}
-install -dv -m 0750 "${SHED_FAKEROOT}/root"
-install -dv -m 1777 "${SHED_FAKEROOT}/tmp" "${SHED_FAKEROOT}/var/tmp"
-mkdir -pv "${SHED_FAKEROOT}"/usr/{,local/}{bin,include,lib,sbin,src}
-mkdir -pv "${SHED_FAKEROOT}"/usr/{,local/}share/{color,dict,doc,info,locale,man}
-mkdir -v  "${SHED_FAKEROOT}"/usr/{,local/}share/{misc,terminfo,zoneinfo}
-mkdir -v  "${SHED_FAKEROOT}"/usr/libexec
-mkdir -pv "${SHED_FAKEROOT}"/usr/{,local/}share/man/man{1..8}
-mkdir -v  "${SHED_FAKEROOT}"/var/{log,mail,spool}
-mkdir -pv "${SHED_FAKEROOT}"/var/{opt,cache,lib/{color,misc,locate},local}
-touch "${SHED_FAKEROOT}"/var/log/{btmp,lastlog,faillog,wtmp}
-chmod -v 664 "${SHED_FAKEROOT}/var/log/lastlog"
-chmod -v 600 "${SHED_FAKEROOT}/var/log/btmp"
-ln -sv ../run "${SHED_FAKEROOT}/var/run"
-ln -sv ../run/lock "${SHED_FAKEROOT}/var/lock"
-ln -sv /proc/self/mounts "${SHED_FAKEROOT}/etc/mtab"
-install -v -m644 "${SHED_CONTRIBDIR}/passwd" "${SHED_FAKEROOT}/etc/passwd.default"
-install -v -m644 "${SHED_CONTRIBDIR}/group" "${SHED_FAKEROOT}/etc/group.default"
+mkdir -pv "${SHED_FAKE_ROOT}"/{media/{floppy,cdrom},sbin,srv,var}
+install -dv -m 0750 "${SHED_FAKE_ROOT}/root"
+install -dv -m 1777 "${SHED_FAKE_ROOT}/tmp" "${SHED_FAKE_ROOT}/var/tmp"
+mkdir -pv "${SHED_FAKE_ROOT}"/usr/{,local/}{bin,include,lib,sbin,src}
+mkdir -pv "${SHED_FAKE_ROOT}"/usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -v  "${SHED_FAKE_ROOT}"/usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -v  "${SHED_FAKE_ROOT}"/usr/libexec
+mkdir -pv "${SHED_FAKE_ROOT}"/usr/{,local/}share/man/man{1..8}
+mkdir -v  "${SHED_FAKE_ROOT}"/var/{log,mail,spool}
+mkdir -pv "${SHED_FAKE_ROOT}"/var/{opt,cache,lib/{color,misc,locate},local}
+touch "${SHED_FAKE_ROOT}"/var/log/{btmp,lastlog,faillog,wtmp}
+chmod -v 664 "${SHED_FAKE_ROOT}/var/log/lastlog"
+chmod -v 600 "${SHED_FAKE_ROOT}/var/log/btmp"
+ln -sv ../run "${SHED_FAKE_ROOT}/var/run"
+ln -sv ../run/lock "${SHED_FAKE_ROOT}/var/lock"
+ln -sv /proc/self/mounts "${SHED_FAKE_ROOT}/etc/mtab"
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/passwd" "${SHED_FAKE_ROOT}/etc/passwd.default"
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/group" "${SHED_FAKE_ROOT}/etc/group.default"
 
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     bootstrap)
         # Install default users and groups
-        install -v -m644 "${SHED_CONTRIBDIR}/passwd" "${SHED_FAKEROOT}/etc/passwd"
-        install -v -m644 "${SHED_CONTRIBDIR}/group" "${SHED_FAKEROOT}/etc/group"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/passwd" "${SHED_FAKE_ROOT}/etc/passwd"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/group" "${SHED_FAKE_ROOT}/etc/group"
 
         # Create temporary symlinks for bootstrap
-        ln -sv /tools/bin/{bash,cat,dd,echo,ln,pwd,rm,stty} "${SHED_FAKEROOT}/bin"
-        ln -sv /tools/bin/{env,install,perl} "${SHED_FAKEROOT}/usr/bin"
-        ln -sv /tools/lib/libgcc_s.so{,.1} "${SHED_FAKEROOT}/usr/lib"
-        ln -sv /tools/lib/libstdc++.{a,so{,.6}} "${SHED_FAKEROOT}/usr/lib"
+        ln -sv /tools/bin/{bash,cat,dd,echo,ln,pwd,rm,stty} "${SHED_FAKE_ROOT}/bin"
+        ln -sv /tools/bin/{env,install,perl} "${SHED_FAKE_ROOT}/usr/bin"
+        ln -sv /tools/lib/libgcc_s.so{,.1} "${SHED_FAKE_ROOT}/usr/lib"
+        ln -sv /tools/lib/libstdc++.{a,so{,.6}} "${SHED_FAKE_ROOT}/usr/lib"
         for SHDPKG_LIB in blkid lzma mount uuid
         do
-            ln -sv /tools/lib/lib${SHDPKG_LIB}.so* "${SHED_FAKEROOT}/usr/lib"
+            ln -sv /tools/lib/lib${SHDPKG_LIB}.so* "${SHED_FAKE_ROOT}/usr/lib"
         done
-        ln -svf /tools/include/blkid "${SHED_FAKEROOT}/usr/include"
-        ln -svf /tools/include/libmount "${SHED_FAKEROOT}/usr/include"
-        ln -svf /tools/include/uuid "${SHED_FAKEROOT}/usr/include"
-        install -vdm755 "${SHED_FAKEROOT}/usr/lib/pkgconfig"
+        ln -svf /tools/include/blkid "${SHED_FAKE_ROOT}/usr/include"
+        ln -svf /tools/include/libmount "${SHED_FAKE_ROOT}/usr/include"
+        ln -svf /tools/include/uuid "${SHED_FAKE_ROOT}/usr/include"
+        install -vdm755 "${SHED_FAKE_ROOT}/usr/lib/pkgconfig"
         for SHDPKG_LIB in blkid mount uuid
         do
             sed 's@tools@usr@g' /tools/lib/pkgconfig/${SHDPKG_LIB}.pc \
-                > "${SHED_FAKEROOT}/usr/lib/pkgconfig/${SHDPKG_LIB}.pc"
+                > "${SHED_FAKE_ROOT}/usr/lib/pkgconfig/${SHDPKG_LIB}.pc"
         done
-        ln -sv bash "${SHED_FAKEROOT}/bin/sh"
+        ln -sv bash "${SHED_FAKE_ROOT}/bin/sh"
         ;;
     release)
         # NOTE: In 'bootstrap' this is done as a post-install step because we need
@@ -62,7 +62,7 @@ case "$SHED_BUILDMODE" in
         # from same the same release series. This allows this package to be installed
         # without bash and its dependencies being available, which running the
         # post-install would require.
-        chgrp -v utmp "${SHED_FAKEROOT}/var/log/lastlog"
+        chgrp -v utmp "${SHED_FAKE_ROOT}/var/log/lastlog"
         ;;
 esac
 
--- postinstall.sh
+++ postinstall.sh
@@ -6,11 +6,23 @@ fi
 if [ ! -e /etc/group ]; then
     install -v -m644 /etc/group.default /etc/group
 fi
-if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
     chgrp -v utmp /var/log/lastlog
-    chgrp -v -R shedmake /var/shedmake
-    chmod -v -R g+s /var/shedmake
-    chmod -v 775 /var/shedmake/repos/local
+    chgrp -v shedmake /var/shedmake
+    chgrp -v shedmake /var/shedmake/template
+    chgrp -v shedmake /var/shedmake/repos
+    chgrp -v shedmake /var/shedmake/repos/remote
+    chgrp -v shedmake /var/shedmake/repos/remote/system
+    chgrp -v shedmake /var/shedmake/repos/local
+    chgrp -v shedmake /var/shedmake/repos/local/default
+    chmod -v -R 775 /var/shedmake/repos/local
+    chmod -v g+s /var/shedmake
+    chmod -v g+s /var/shedmake/template
+    chmod -v g+s /var/shedmake/repos
+    chmod -v g+s /var/shedmake/repos/remote
+    chmod -v g+s /var/shedmake/repos/remote/system
+    chmod -v g+s /var/shedmake/repos/local
+    chmod -v g+s /var/shedmake/repos/local/default
 fi
 # HACK: Clean up duplicate symlink that results from installing over an existing system
 if [ -L /run/run ]; then
