93cc57c9439224d43464badbf7f3794c7f3e4dc9
Auston Stewart <auston@shedbuilt.net>
2018-04-16 09:11:53 -1000

    Adding configuration files for blank

--- build.sh
+++ build.sh
@@ -14,8 +14,7 @@ mkdir -pv "${SHED_FAKE_ROOT}"/{media/{floppy,cdrom},sbin,srv,var}
 install -dv -m 0750 "${SHED_FAKE_ROOT}/root"
 install -dv -m 1777 "${SHED_FAKE_ROOT}/tmp" "${SHED_FAKE_ROOT}/var/tmp"
 mkdir -pv "${SHED_FAKE_ROOT}"/usr/{,local/}{bin,include,lib,sbin,src}
-mkdir -pv "${SHED_FAKE_ROOT}"/usr/{,local/}share/{color,dict,doc,info,locale,man}
-mkdir -v  "${SHED_FAKE_ROOT}"/usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -pv "${SHED_FAKE_ROOT}"/usr/{,local/}share/{color,defaults,dict,doc,info,locale,man,misc,terminfo,zoneinfo}
 mkdir -v  "${SHED_FAKE_ROOT}"/usr/libexec
 mkdir -pv "${SHED_FAKE_ROOT}"/usr/{,local/}share/man/man{1..8}
 mkdir -v  "${SHED_FAKE_ROOT}"/var/{log,mail,spool}
@@ -26,14 +25,30 @@ chmod -v 600 "${SHED_FAKE_ROOT}/var/log/btmp"
 ln -sv ../run "${SHED_FAKE_ROOT}/var/run"
 ln -sv ../run/lock "${SHED_FAKE_ROOT}/var/lock"
 ln -sv /proc/self/mounts "${SHED_FAKE_ROOT}/etc/mtab"
-install -v -m644 "${SHED_PKG_CONTRIB_DIR}/passwd" "${SHED_FAKE_ROOT}/etc/passwd.default"
-install -v -m644 "${SHED_PKG_CONTRIB_DIR}/group" "${SHED_FAKE_ROOT}/etc/group.default"
+# Distribution info and LSB compliance
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/os-release" "${SHED_FAKE_ROOT}/etc"
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/lsb-release" "${SHED_FAKE_ROOT}/etc"
+# Default users and groups
+install -dm755 "${SHED_FAKE_ROOT}/usr/share/defaults/etc"
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/passwd" "${SHED_FAKE_ROOT}/usr/share/defaults/etc"
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/group" "${SHED_FAKE_ROOT}/usr/share/defaults/etc"
+# Default hostname and hosts
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/hostname" "${SHED_FAKE_ROOT}/usr/share/defaults/etc"
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/hosts" "${SHED_FAKE_ROOT}/usr/share/defaults/etc"
+# Default automount config
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/fstab" "${SHED_FAKE_ROOT}/usr/share/defaults/etc"
+# Default login prompt config
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/issue" "${SHED_FAKE_ROOT}/usr/share/defaults/etc"
 
 case "$SHED_BUILD_MODE" in
     bootstrap)
         # Install default users and groups
-        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/passwd" "${SHED_FAKE_ROOT}/etc/passwd"
-        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/group" "${SHED_FAKE_ROOT}/etc/group"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/passwd" "${SHED_FAKE_ROOT}/etc"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/group" "${SHED_FAKE_ROOT}/etc"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/hostname" "${SHED_FAKE_ROOT}/etc"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/hosts" "${SHED_FAKE_ROOT}/etc"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/fstab" "${SHED_FAKE_ROOT}/etc"
+        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/issue" "${SHED_FAKE_ROOT}/etc"
 
         # Create temporary symlinks for bootstrap
         ln -sv /tools/bin/{bash,cat,dd,echo,ln,pwd,rm,stty} "${SHED_FAKE_ROOT}/bin"
--- postinstall.sh
+++ postinstall.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# Install default passwd and group files, if necessary
-if [ ! -e /etc/passwd ]; then
-    install -v -m644 /etc/passwd.default /etc/passwd
+if [ ! -e /etc/hostname ]; then
+    install -v -m644 /usr/share/defaults/etc/hostname /etc/hostname
 fi
-if [ ! -e /etc/group ]; then
-    install -v -m644 /etc/group.default /etc/group
+if [ ! -e /etc/hosts ]; then
+    install -v -m644 /usr/share/defaults/etc/hosts /etc/hosts
+fi
+if [ ! -e /etc/fstab ]; then
+    install -v -m644 /usr/share/defaults/etc/fstab /etc/fstab
+fi
+if [ ! -e /etc/issue ]; then
+    install -v -m644 /usr/share/defaults/etc/issue /etc/issue
 fi
 if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
     chgrp -v utmp /var/log/lastlog
+    chmod -v 775 /var/tmp/shedmake
+    chgrp -v shedmake /var/tmp/shedmake
     chgrp -v shedmake /var/shedmake
     chgrp -v shedmake /var/shedmake/template
     chgrp -v shedmake /var/shedmake/repos
     chgrp -v shedmake /var/shedmake/repos/remote
-    chgrp -v shedmake /var/shedmake/repos/remote/system
     chgrp -v shedmake /var/shedmake/repos/local
     chgrp -v shedmake /var/shedmake/repos/local/default
     chmod -v -R 775 /var/shedmake/repos/local
@@ -20,7 +26,6 @@ if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
     chmod -v g+s /var/shedmake/template
     chmod -v g+s /var/shedmake/repos
     chmod -v g+s /var/shedmake/repos/remote
-    chmod -v g+s /var/shedmake/repos/remote/system
     chmod -v g+s /var/shedmake/repos/local
     chmod -v g+s /var/shedmake/repos/local/default
 fi
